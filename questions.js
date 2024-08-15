39f1450a8dc5ec89ea00a0d4ec2766ac1686197e
ChunHan08 <149086722+ChunHan08@users.noreply.github.com>
2024-08-15 02:19:43 +0000

    update

---
+++ questions.js
@@ -0,0 +1,121 @@
+let questions = [
+  {
+    numb: 1,
+    question: "What song is this lyric from: I see your face in my mind as I drive away?",
+    answer: "breathe",
+    options: [
+      "breathe",
+      "Come In With the rain",
+      "tell me why",
+      "fearless",
+    ]
+  },
+  {
+    numb: 2,
+    question: "What song is this lyric from: Hang your head low?",
+    answer: "Cruel Summer",
+    options: [
+      "Cruel Summer",
+      "All Of The Girls You Loved Before",
+      "London Boy",
+      "I Think He Knows",
+    ]
+  },
+  {
+    numb: 3,
+    question: "What song is this lyric from: ",
+    answer: "Cruel Summer",
+    options: [
+      "Cruel Summer",
+      "All Of The Girls You Loved Before",
+      "London Boy",
+      "I Think He Knows",
+    ]
+  },
+  {
+    numb: 4,
+    question: "What song is this lyric from: Don’t read the last page?",
+    answer: "New Years Day",
+    options: [
+      "Call it what you want",
+      "New Years Day",
+      "Delicate",
+      "So it Goes",
+    ]
+  },
+  { numb: 5,
+    question: "What song is this lyric from: you just see right through me?",
+    answer: "Invisible",
+    options: [
+      "Invisible",
+      "Picture to Burn",
+      "Tim McGraw",
+      "Should've Said No",
+    ]
+    },
+  {
+    numb: 6,
+    question: "What song is this lyric from: It rains when you're here and it rains when you're gone.",
+    answer: "Forever & Always",
+    options: [
+      "Come in with the rain",
+      "Forever & Always",
+      "If this was a movie",
+      "Fearless",
+    ]
+  },
+  {
+    numb: 7,
+    question: "What song is this lyric from: Is this the end of all the endings?",
+    answer: "King Of My Heart",
+    options: [
+      "End Game",
+      "Dont't Blame Me",
+      "New Years Day",
+      "King Of My Heart",
+    ]
+  },
+  {
+    numb: 8,
+    question: "What song is this lyric from: I walked in expecting you'd be late.",
+    answer: "Begin Again",
+    options: [
+      "Treacherous",
+      "Begin Again",
+      "Girl At Home",
+      "The Last Time",
+    ]
+  },
+  {
+    numb: 9,
+    question: "What song is this lyric from: Drank My posion all alone.",
+    answer: "The Great War",
+    options: [
+      "The Great War",
+      "Paris",
+      "Sweet nothing",
+      "Hits diffrent"
+    ]
+  },
+  {
+    numb: 10,
+    question: "What song is this lyric from: You know I left a part of me back in New York.",
+    answer: "Hoax",
+    options: [
+      "Hoax",
+      "Mirrorball",
+      "Epiphany",
+      "Seven",
+    ]
+  },
+  {
+    numb: 11,
+    question: "What song is this lyric from: But i felt a hole like this.",
+    answer: "loml",
+    options: [
+      "imgonnagetyouback",
+      "loml",
+      "Clara Bow",
+      "The Alchemy",
+    ]
+  },
